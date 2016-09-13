50964db9af9a33709e4309eff64da842c8d79ab6
Philip Preston <phpreston@ebay.com>
2016-09-13 15:01:47 -0400

    Order Correction feed in, general clean up

---
+++ src/app/code/Pepperjam/Network/Helper/Map.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pepperjam\Network\Helper;
+
+use \Magento\Framework\App\Helper\AbstractHelper;
+use \Magento\Framework\App\Helper\Context;
+
+class Map extends AbstractHelper {
+	public function __construct(Context $context) {
+		parent::__construct($context);
+	}
+}
