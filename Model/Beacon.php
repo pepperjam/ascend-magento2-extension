1bc76bb2f611bfd69323c88b33b02b8529cbf0c6
PEPPERJAM\ppreston <ppreston@corp.pepperjam.com>
2017-06-14 13:45:32 -0400

    Merge branch 'master' of bitbucket.org:pepperjam/pepperjam-network-magento2-extension

--- Model/Beacon.php
+++ Model/Beacon.php
@@ -4,16 +4,24 @@ namespace Pepperjam\Network\Model;
 use Pepperjam\Network\Helper\Data;
 use Pepperjam\Network\Helper\Config;
 
+use Magento\Checkout\Model\Session;
+use Magento\Sales\Model\OrderFactory;
+
 abstract class Beacon
 {
+    protected $checkoutSession;
     protected $config;
     protected $helper;
     protected $order;
 
-    public function __construct(Config $config, Data $helper)
+    public function __construct(Config $config, Data $helper, OrderFactory $orderFactory, Session $checkoutSession)
     {
+        $this->checkoutSession = $checkoutSession;
         $this->config = $config;
         $this->helper = $helper;
+
+        $lastOrderId = $checkoutSession->getLastRealOrderId();
+        $this->order = $orderFactory->create()->loadByIncrementId($lastOrderId);
     }
 
     public function setOrder($order)
@@ -24,7 +32,7 @@ abstract class Beacon
     protected function getCouponCode($params)
     {
         if (trim($this->order->getCouponCode()) != '') {
-            $params[$this->couponKey] = trim($order->getCouponCode());
+            $params[$this->couponKey] = trim($this->order->getCouponCode());
         }
 
         return $params;
