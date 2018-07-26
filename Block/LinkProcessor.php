06e53eeed7877f5f73cd307c4e5eb652e73d42c7
Juan Borla <jborla@corp.pepperjam.com>
2018-07-26 14:58:35 -0400

    [WIP] Support Safari ITP (AD-2819)
    Add Block to read URL and store utm_campaign value in session
    TODO: Add value to Beacon's URL

---
+++ Block/LinkProcessor.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pepperjam\Network\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Pepperjam\Network\Helper\LinkHelper;
+
+class LinkProcessor extends Template {
+
+	protected $helper;
+
+	public function __construct(
+		Context $context,
+		array $data = [],
+		LinkHelper $helper
+	) {
+		$this->helper = $helper;
+
+		parent::__construct($context, $data);
+	}
+
+	public function readUrl() {
+		$this->helper->readUrl();
+	}
+}
