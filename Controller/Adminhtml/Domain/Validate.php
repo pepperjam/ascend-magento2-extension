3cb53fef2acae76e48a0a2f4501c7db241468ff1
Dorin Mirulescu <dorin.mirulescu@rocketweb.com>
2024-04-10 11:16:06 +0300

    Input sanitization to strengthen security around SSRF attacks.

--- Controller/Adminhtml/Domain/Validate.php
+++ Controller/Adminhtml/Domain/Validate.php
@@ -28,19 +28,27 @@ class Validate extends Action
      */
     public function execute()
     {
-        $return = true;
+        $return = ['success' => true, 'message' => ''];
         try {
             $domain = rtrim($this->getRequest()->getParam('url'),"/");
-            $ch = curl_init('https://'. $domain. '/whatismyname');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-            curl_setopt($ch, CURLOPT_TIMEOUT,100);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            $resp = curl_exec($ch);
-            $retCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-            if (!in_array($retCode, ['200', '301', '302']) || trim(trim($resp)) != $domain) {
-                $return = false;
+            $regex = '/^(?!:\/\/)(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/';
+            if (preg_match($regex, $domain)) {
+                $ch = curl_init('https://' . $domain . '/whatismyname');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                $resp = curl_exec($ch);
+                $retCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                if (!in_array($retCode, ['200', '301', '302']) || trim(trim($resp)) != $domain) {
+                    $return = ['success' => false,
+                        'message' => "Your custom domain has not been properly provisioned in Ascend.
+                                      Log in <a href=\"https://ascend.pepperjam.com/merchant/integration/tracking-domains\">here</a>
+                                      to complete the provisioning"];
+                }
+            } else {
+                $return = ['success' => false, 'message' => "Not a valid URL"];
             }
         } catch (\Exception $e) {
             $this->_objectManager->get('Psr\Log\LoggerInterface')->critical($e);
@@ -48,7 +56,7 @@ class Validate extends Action
 
         /** @var \Magento\Framework\Controller\Result\Json $result */
         $result = $this->resultJsonFactory->create();
-        return $result->setData(['success' => $return]);
+        return $result->setData($return);
     }
 
     protected function _isAllowed()
